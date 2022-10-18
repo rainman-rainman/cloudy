9af2906b771d2b6134538d8ac7c46508dd6631ca
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 22:32:43 +0200

    extension loading debug commands

--- bot.py
+++ bot.py
@@ -3,6 +3,24 @@ from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
 bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
 load_dotenv(find_dotenv())
+@bot.command()
+async def load(ctx, extension):
+  bot.load_extension(f'modules.{extension}')
+  print(f'{extension} successfully loaded')
+  ctx.send(f'{extension} successfully loaded')
+# cog unloader command
+@bot.command()
+async def unload(ctx, extension):
+  bot.unload_extension(f'modules.{extension}')
+  print(f'{extension} successfully unloaded')
+  ctx.send(f'{extension} successfully unloaded')
+# cog reloader command, unload then load extenion
+@bot.command()
+async def reload(ctx, extension):
+  bot.unload_extension(f'modules.{extension}')
+  bot.load_extension(f'modules.{extension}')
+  print(f'{extension} successfully re-loaded')
+  ctx.send(f'{extension} successfully re-loaded')
 # for loop to find cogs folder
 for filename in os.listdir('./modules'):
   if filename.endswith('.py'):
