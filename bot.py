761511600cebc6f52ca1e428d87d18a55a023f4e
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 22:42:59 +0200

    Module dir name can now be configured from the .env config & debug modules folder

--- bot.py
+++ bot.py
@@ -2,27 +2,10 @@ import discord,os
 from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
 bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
+modules_dir={os.environ.get("MODULES_FOLDER_NAME")}
 load_dotenv(find_dotenv())
-@bot.command()
-async def load(ctx, extension):
-  bot.load_extension(f'modules.{extension}')
-  print(f'{extension} successfully loaded')
-  ctx.send(f'{extension} successfully loaded')
-# cog unloader command
-@bot.command()
-async def unload(ctx, extension):
-  bot.unload_extension(f'modules.{extension}')
-  print(f'{extension} successfully unloaded')
-  ctx.send(f'{extension} successfully unloaded')
-# cog reloader command, unload then load extenion
-@bot.command()
-async def reload(ctx, extension):
-  bot.unload_extension(f'modules.{extension}')
-  bot.load_extension(f'modules.{extension}')
-  print(f'{extension} successfully re-loaded')
-  ctx.send(f'{extension} successfully re-loaded')
-# for loop to find cogs folder
-for filename in os.listdir('./modules'):
+for filename in os.listdir(f'./{modules_dir}'):
   if filename.endswith('.py'):
-    bot.load_extension(f'modules.{filename[:-3]}')
+    bot.load_extension(f'{modules_dir}.{filename[:-3]}')
+    print(f'[SETUP ⚙️]: loaded module {filename[:-3]} from {modules_dir} 🚀')
 bot.run(os.environ.get("TOKEN"))
---
+++ modules2/foo.py
@@ -0,0 +1,9 @@
+from discord.ext import commands
+class MyCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    @commands.command()
+    async def baz(self, ctx):
+        await ctx.send("something")
+def setup(bot):
+    bot.add_cog(MyCog(bot))
