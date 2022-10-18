48c288022d5e53c9f44cc8073066d0d69ff72fbe
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 23:32:03 +0200

    multiple attempts at getting the cogs & extensions architecture to work

--- bot.py
+++ bot.py
@@ -1,13 +1,17 @@
 import discord,os
 from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
-bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
+bot=commands.Bot(command_prefix='c!',intents=discord.Intents.all())
+@bot.event
+async def on_ready():
+    print("[SETUP ⚙️ ]: bot is ready to rock & roll 🚀")
 load_dotenv(find_dotenv())
 modules_dir=os.environ.get("MODULES_FOLDER_NAME")
-print(f'[SETUP ⚙️]: modules directory name is {modules_dir}.')
-async def setup():
-    for filename in os.listdir('./'+modules_dir):
-        if filename.endswith('.py'):
-            await bot.load_extension(f'{modules_dir}.{filename[:-3]}')
-            print(f'[SETUP ⚙️]: loaded module {filename[:-3]} from {modules_dir} 🚀')
+print(f'[SETUP ⚙️ ]: modules directory name is {modules_dir}.')
+async def load(extension):
+    await bot.load_extension(extension)
+for filename in os.listdir('./'+modules_dir):
+    if filename.endswith('.py'):
+        load(modules_dir+'.'+filename[:-3])
+        print(f'[SETUP ⚙️ ]: loaded module {filename[:-3]} from {modules_dir} 🚀')
 bot.run(os.environ.get("TOKEN"))
--- modules_2/foo.py
+++ modules_2/foo.py
@@ -1,9 +1,9 @@
 from discord.ext import commands
 class MyCog(commands.Cog):
-    def __init__(self, bot):
-        self.bot = bot
-    @commands.command()
-    async def baz(self, ctx):
+    def __init__(self,bot):
+        self.bot=bot
+    @commands.command('baz')
+    async def b(self,ctx):
         await ctx.send("something")
 def setup(bot):
     bot.add_cog(MyCog(bot))
