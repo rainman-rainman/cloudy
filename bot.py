3381d3d25596c38cf11b8712566a067615098a24
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 22:44:17 +0200

    dotenv load fix

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import discord,os
 from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
 bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
-modules_dir={os.environ.get("MODULES_FOLDER_NAME")}
+modules_dir=os.environ.get("MODULES_FOLDER_NAME")
 load_dotenv(find_dotenv())
 for filename in os.listdir(f'./{modules_dir}'):
   if filename.endswith('.py'):
