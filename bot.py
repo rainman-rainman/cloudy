c6464cd947d10c9fe21d7eec0d549630790d7c24
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 22:50:17 +0200

    lmao load_dotenv was executed after environ.get

--- bot.py
+++ bot.py
@@ -2,10 +2,12 @@ import discord,os
 from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
 bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
-modules_dir=os.environ.get("MODULES_FOLDER_NAME")
 load_dotenv(find_dotenv())
-for filename in os.listdir(f'./{modules_dir}'):
-  if filename.endswith('.py'):
-    bot.load_extension(f'{modules_dir}.{filename[:-3]}')
-    print(f'[SETUP ⚙️]: loaded module {filename[:-3]} from {modules_dir} 🚀')
+modules_dir=os.environ.get("MODULES_FOLDER_NAME")
+print(f'[SETUP ⚙️]: modules directory name is {modules_dir}.')
+async def setup():
+    for filename in os.listdir('./'+modules_dir):
+        if filename.endswith('.py'):
+            await bot.load_extension(f'{modules_dir}.{filename[:-3]}')
+            print(f'[SETUP ⚙️]: loaded module {filename[:-3]} from {modules_dir} 🚀')
 bot.run(os.environ.get("TOKEN"))
