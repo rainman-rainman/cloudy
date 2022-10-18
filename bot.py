d53599ff2a4e51a0927262ce8cfa4a4c829a7cba
Artefakt-Online <110351742+Artefakt-Online@users.noreply.github.com>
2022-10-18 22:22:14 +0200

    a more dynamic way to load all modules from the modules folder

--- bot.py
+++ bot.py
@@ -3,7 +3,8 @@ from dotenv import load_dotenv,find_dotenv
 from discord.ext import commands
 bot=commands.Bot(command_prefix='.',intents=discord.Intents.all())
 load_dotenv(find_dotenv())
-bot.load_extension("modules.music")
-bot.load_extension("modules.documentation")
-bot.load_extension("modules.moderation")
+# for loop to find cogs folder
+for filename in os.listdir('./modules'):
+  if filename.endswith('.py'):
+    bot.load_extension(f'modules.{filename[:-3]}')
 bot.run(os.environ.get("TOKEN"))
